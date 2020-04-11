a06edebcd0a801bbd9df381a4465b99c6243f6fc
Remi Mestdagh <remi.mestdagh@student.hogent.be>
2020-04-11 15:14:00 +0200

    implementatie feedbacktest

---
+++ src/testen/FeedbackTest.java
@@ -0,0 +1,34 @@
+package testen;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import domein.Feedback;
+
+public class FeedbackTest {
+	private Feedback feedback1;
+	
+	@BeforeEach
+	public void before() {
+		feedback1 = new Feedback("Boris Bever","Super interessante sessie");	
+	}
+	@ParameterizedTest
+	@ValueSource(strings= {" ","","    "})
+	public void legeInhoudGeeftException(String inhoud) {
+		Assertions.assertThrows(IllegalArgumentException.class, ()->{
+			new Feedback("Boris Bever",inhoud);
+		});
+		
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings= {" ","","    "})
+	public void legeAuteurGeeftException(String auteur) {
+		Assertions.assertThrows(IllegalArgumentException.class, ()->{
+			new Feedback(auteur,"Super interessant!");
+		});
+		
+	}
+}

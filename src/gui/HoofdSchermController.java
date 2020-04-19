40e0b924b83b03543b4cd1009e686b2ebcd2f8e3
Bert Suffys <bert.suffys@student.hogent.be>
2020-04-19 16:19:34 +0200

    button hoverover tooltips & meer opmaak

--- src/gui/HoofdSchermController.java
+++ src/gui/HoofdSchermController.java
@@ -6,6 +6,7 @@ import java.util.ResourceBundle;
 
 import domein.DomeinController;
 import javafx.application.Platform;
+import javafx.css.Style;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -16,9 +17,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class HoofdSchermController extends SchermController implements Initializable{
 	
@@ -36,7 +39,7 @@ public class HoofdSchermController extends SchermController implements Initializ
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		
+		initializeButtonTooltips();
 	}
 
 	@Override
@@ -67,6 +70,38 @@ public class HoofdSchermController extends SchermController implements Initializ
         }else {
         	verranderScherm(btnKalender, "SessieKalender");
         }
+	}
+	
+	
+	private void initializeButtonTooltips()
+	{
+		//styling
+		String tooltipStyling = "-fx-background: rgba(200,200,200,1);\r\n" + 
+				"    -fx-text-fill: white;\r\n" + 
+				"    -fx-background-color: rgba(255,255,255,0.1);\r\n" + 
+				"    -fx-background-radius: 0px;\r\n" + 
+				"	 -fx-border-color: white;\r\n" + 
+				"    -fx-border-width: 1px;\r\n"+
+				"    -fx-padding: 0.667em 0.75em 0.667em 0.75em; /* 10px */\r\n" + 
+				"    -fx-font-size: 0.85em;";
+		
+		//button voor beheer gebruikers
+		Tooltip tooltipBeheerGebruikers = new Tooltip("Hier kunt u de gegevens van bestaande gebruikers aanpassen.");
+		btnBeheerGebruikers.setTooltip(tooltipBeheerGebruikers);
+		tooltipBeheerGebruikers.setShowDelay(Duration.seconds(0.2));
+		tooltipBeheerGebruikers.setStyle(tooltipStyling);
+		
+		//button voor beheer statistieken
+		Tooltip tooltipStatistieken = new Tooltip("Hier kunt u gebruikers, aanwezigheden per sessie en een PowerBI overzicht bekijken.");
+		btnStat.setTooltip(tooltipStatistieken);
+		tooltipStatistieken.setShowDelay(Duration.seconds(0.2));
+		tooltipStatistieken.setStyle(tooltipStyling);
+		
+		//button voor beheer gebruikers
+		Tooltip tooltipKalender = new Tooltip("Hier kunt u sessiekalenders aanpassen, aanmaken en verwijderen alsook de daartoe horende sessies beheren.");
+		btnKalender.setTooltip(tooltipKalender);
+		tooltipKalender.setShowDelay(Duration.seconds(0.2));
+		tooltipKalender.setStyle(tooltipStyling);
 	}
 	
 }
